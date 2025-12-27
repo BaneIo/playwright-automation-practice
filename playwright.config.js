9836b8b8215d15044dad0835e39a5d074b057b6a
Branko <branko.askovic.io@gmail.com>
2025-12-27 23:27:20 +0100

    Configure Playwright Allure + GitHub Pages

--- playwright.config.js
+++ playwright.config.js
@@ -1,51 +1,54 @@
-// @ts-check
 import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// import dotenv from "dotenv";
+// import path from "path";
+// dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
   testDir: "./tests",
+
   /* Run tests in files in parallel */
   fullyParallel: false,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
-  //retries: 1,
+  // retries: 1,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: "html",
-  //reporter: [['json', {outputfile: 'results.json'}]],
-  //reporter: [["junit", { outputFile: "results.xml" }]],
 
+  /* Reporter configuration */
   reporter: [
     ["list"],
-    ["html"],
-    ["junit", { outputFile: "results.xml" }],
-    ["json", { outputFile: "results.json" }],
+    // ["html"],
+    // ["junit", { outputFile: "results.xml" }],
+    // ["json", { outputFile: "results.json" }],
     ["allure-playwright", { outputFolder: "allure-results" }],
   ],
 
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  /* Shared settings for all the projects below */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    // baseURL: "http://localhost:3000",
+    /* Collect trace when retrying the failed test. */
     // trace: "retain-on-failure",
+    /* Take screenshot only on failure */
     // screenshot: "only-on-failure",
-    //video: "on",
+    /* Record video */
+    // video: "on",
   },
-  //timeout: 1000,
+
+  // timeout: 1000,
 
   /* Configure projects for major browsers */
   projects: [
@@ -53,6 +56,7 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
+
     /*
     {
       name: "firefox",
@@ -63,32 +67,33 @@ export default defineConfig({
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
-*/
+    */
+
     /* Test against mobile viewports. */
     // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
+    //   name: "Mobile Chrome",
+    //   use: { ...devices["Pixel 5"] },
     // },
     // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
+    //   name: "Mobile Safari",
+    //   use: { ...devices["iPhone 12"] },
     // },
 
     /* Test against branded browsers. */
     // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    //   name: "Microsoft Edge",
+    //   use: { ...devices["Desktop Edge"], channel: "msedge" },
     // },
     // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    //   name: "Google Chrome",
+    //   use: { ...devices["Desktop Chrome"], channel: "chrome" },
     // },
   ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
+  //   command: "npm run start",
+  //   url: "http://localhost:3000",
   //   reuseExistingServer: !process.env.CI,
   // },
 });
